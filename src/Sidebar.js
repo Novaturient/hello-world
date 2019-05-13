43e32d7d0cdaef56a6da8169760bfccc70a7d5a2
Novaturient <rofel.valeros@yahoo.com>
2019-05-13 15:19:03 +0800

    Added icons in the sidenav

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloudSunRain, faHome, faWater, faWind, faTemperatureHigh, faTemperatureLow} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCloudSunRain, faHome, faWater, faWind, faTemperatureHigh, faTemperatureLow);
+
 
 function Sidebar() {
     return (
@@ -10,39 +18,58 @@ function Sidebar() {
                     // Add your code here
                 }}
             >
-                <SideNav.Toggle />
-                <SideNav.Nav defaultSelected="home">
-                    <NavItem eventKey="home">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
+            
+            <SideNav.Toggle />
+            <SideNav.Nav>
+                <NavItem eventKey="home" >
+                    <NavIcon>
+                        {/*<i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} /> */}
+                        <FontAwesomeIcon icon={faHome} style={{ fontSize: '1.80em' }}/>  
+                    </NavIcon>
+                    <NavText>
+                        Home
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey="charts">
+                    <NavIcon>
+                        {/*<i className="fa fa-sun-o" style={{ fontSize: '1.75em' }} />*/}
+                        <FontAwesomeIcon icon={faCloudSunRain} style={{ fontSize: '1.80em' }}/>
+                    </NavIcon>
+                    <NavText>
+                        Weather
+                    </NavText>
+
+                    <NavItem eventKey="charts/highTemp">
                         <NavText>
-                            Home
-            </NavText>
+                            High Temperature
+                        </NavText>
                     </NavItem>
-                    <NavItem eventKey="charts">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
+                    <NavItem eventKey="charts/barchart">
                         <NavText>
-                            Charts
-            </NavText>
-                        <NavItem eventKey="charts/linechart">
-                            <NavText>
-                                Line Chart
-                </NavText>
-                        </NavItem>
-                        <NavItem eventKey="charts/barchart">
-                            <NavText>
-                                Bar Chart
-                </NavText>
-                        </NavItem>
+                            Low Temperature
+                        </NavText>
                     </NavItem>
-                </SideNav.Nav>
+                </NavItem>
+                <NavItem eventKey="flood" >
+                    <NavIcon>
+                        <FontAwesomeIcon icon={faWater} style={{ fontSize: '1.80em' }}/> 
+                    </NavIcon>
+                    <NavText>
+                        Flood Alerts
+                    </NavText>
+                </NavItem>
+                <NavItem eventKey="wind" >
+                    <NavIcon>
+                        <FontAwesomeIcon icon={faWind} style={{ fontSize: '1.75em' }}/>
+                    </NavIcon>
+                    <NavText>
+                        Wind Pressure
+                    </NavText>
+                </NavItem>
+            </SideNav.Nav>
             </SideNav>
         </div>
     );
-
 }
 
 export default Sidebar;
